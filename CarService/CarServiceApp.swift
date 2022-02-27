3f96a6c1a8219f2fb937b8c649576236a0515045
Sam Tawfik <sam.tawfik@icloud.com>
2022-02-26 23:32:56 -0800

    Initial Commit

---
+++ CarService/CarServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarServiceApp.swift
+//  CarService
+//
+//  Created by sam on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CarServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
